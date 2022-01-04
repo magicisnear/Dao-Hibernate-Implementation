a8351167051a4cc2268a7322b6d9c97c62126b40
Vasilevich_Y <magicisnear@bk.ru>
2022-01-04 17:22:23 +0300

    new

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -3,7 +3,7 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
-import javax.xml.transform.Result;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -24,7 +24,7 @@ public class UserDaoJDBCImpl implements UserDao {
                     "    , last_name VARCHAR(30) NOT NULL\n" +
                     "    , age int\n" +
                     ");";
-            statement.executeQuery(SQL);
+            statement.executeUpdate(SQL);
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -34,17 +34,35 @@ public class UserDaoJDBCImpl implements UserDao {
         try {
             Statement statement = Util.connection.createStatement();
             String SQL = "DROP TABLE USER";
-            statement.executeQuery(SQL);
+            statement.executeUpdate(SQL);
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        try {
+            PreparedStatement preparedStatement = Util.connection.prepareStatement("INSERT  INTO USER VALUES(?,?,?)");
 
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setInt(1, age);
+
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
+        try {
+            PreparedStatement preparedStatement = Util.connection.prepareStatement("DELETE * FROM USER Where id=?");
+            preparedStatement.setInt(1, (int) id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
@@ -72,6 +90,18 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void cleanUsersTable() {
+        try {
+            Statement statement = Util.connection.createStatement();
+            String SQL = "SELECT * FROM USER";
+            ResultSet resultSet = statement.executeQuery(SQL);
+            while (resultSet.next()) {
+                removeUserById(resultSet.getInt("id"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
+
 }
+
