e015b54c737c1b2c078c0c17fbe532eb980cb170
Vasilevich_Y <magicisnear@bk.ru>
2022-01-04 14:50:34 +0300

    new

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,15 +1,29 @@
 package jm.task.core.jdbc.util;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Util {
-    String url = "";
-    String login = "";
-    String pass = "";
+    private  static  final String URL = "jdbc:mysql://localhost:3306";
+    private  static  final String USERNAME = "root";
+    private  static  final String PASSWORD = "root";
+    private static Connection connection;
 
-    getClass().forname();
+    static  {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
 
-    try(Connection connect = new Connection()) {
-    })
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 
 }
