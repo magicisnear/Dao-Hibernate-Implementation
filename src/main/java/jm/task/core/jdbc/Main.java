d1fc25ebc3cf7be52638291c26f0b37c620340b3
Vasilevich_Y <magicisnear@bk.ru>
2022-01-04 18:49:17 +0300

    new

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,11 +1,12 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
-        UserServiceImpl impl = new UserServiceImpl();
+        UserDaoJDBCImpl impl = new UserDaoJDBCImpl();
         impl.createUsersTable();
         impl.saveUser("Jhon", "Karabutovich", (byte) 16);
         impl.saveUser("Ded", "Dedovich", (byte) 48);
